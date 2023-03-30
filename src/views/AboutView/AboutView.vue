eaef812e9142f7cf3a33c47b7593c467c2366b4f
xiangnan.li <xiangnan.li@transwarp.io>
2023-03-30 18:05:37 +0800

    my vue separate js module

--- src/views/AboutView/AboutView.vue
+++ src/views/AboutView/AboutView.vue
@@ -8,23 +8,23 @@
   </div>
 </template>
 
-<script lang="ts">
-export default {
-  data() {
-    return {
-      count: 0
-    }
-  },
-  methods: {
-    increment() {
-      this.count++
-    }
-  },
-  mounted() {
-    // 在其他方法或是生命周期中也可以调用方法
-    this.increment()
-  }
-}
+<script src="./AboutView.js">
+// export default {
+//   data() {
+//     return {
+//       count: 0
+//     }
+//   },
+//   methods: {
+//     increment() {
+//       this.count++
+//     }
+//   },
+//   mounted() {
+//     // 在其他方法或是生命周期中也可以调用方法
+//     this.increment()
+//   }
+// }
 </script>
 
 <style src="./AboutView.css"></style>
